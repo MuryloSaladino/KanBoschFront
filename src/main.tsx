c96fbf850a93fac001d48bdb066ba0996dabd69b
MuryloSaladino <murylosaladino@gmail.com>
2024-09-16 09:06:44 -0300

    initial structure

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+
+import "./styles/reset.css"
+import "./styles/global.css"
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      
+    </StrictMode>,
+)
