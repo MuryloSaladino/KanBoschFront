2845f2b05547f992fe956a7772cba609e583b36b
MuryloSaladino <murylosaladino@gmail.com>
2024-09-18 13:58:52 -0300

    open home

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/public/Login";
 import ProtectedRoute from "../components/ProtectedRoute";
-import Home from "../pages/protected/Home";
+import Home from "../pages/public/Home";
 import { UserProvider } from "../providers/UserProvider";
 import NotFound from "../pages/public/NotFound";
 import Register from "../pages/public/Register";
@@ -24,18 +24,17 @@ const mainRouter = createBrowserRouter([
         element: <Register/>
     },
     {
+        path: "home",
+        element: <Home/>
+    },
+    {
         path: "/",
         element: (
             <UserProvider> 
                 <ProtectedRoute/> 
             </UserProvider>
         ),
-        children: [
-            {
-                path: "home",
-                element: <Home/>
-            },
-        ]
+        children: []
     }
 ])
 
