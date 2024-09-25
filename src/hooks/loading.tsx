617ad6be1efcf42e5cb283c24b0c0743972c13e8
MuryloSaladino <murylosaladino@gmail.com>
2024-09-25 16:05:52 -0300

    loading hook

--- src/hooks/loading.tsx
+++ src/hooks/loading.tsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
 
-export default function useLoading<TReturn>(asyncCallback:(...args:any[]) => Promise<TReturn>) {
+export default function useLoading<TReturn>(
+    asyncCallback:(...args:Parameters<(...args:any) => any>) => Promise<TReturn>,
+    onError?:(err:Error) => void
+) {
 
     const [loading, setLoading] = useState(false);
 
-    const loadCallback = async () => {
+    const loadCallback = async (...args:Parameters<typeof asyncCallback>): Promise<TReturn | undefined> => {
         setLoading(true);
-        const response = await asyncCallback();
-        setLoading(false);
-        return response;
+        try {
+            const response = await asyncCallback(...args);
+            return response;
+        } catch (error) {
+            if(onError && error instanceof Error)
+                onError(error)
+        } finally {
+            setLoading(false);
+        }
+        return;
     }
 
     return { loading, loadCallback }
