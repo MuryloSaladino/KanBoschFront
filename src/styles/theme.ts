fac8251dd2ced38bd74bb8a7b2c635830c05f715
MuryloSaladino <murylosaladino@gmail.com>
2024-09-25 10:53:57 -0300

    icon

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,13 @@
 import { createTheme } from "@mui/material";
 
 const appTheme = createTheme({
-    
+    palette: {
+        primary: {
+            main: "#00B4D8",
+            light: "#90E0EF",
+            dark: "#03045E"
+        },
+    }
 })
 
 export default appTheme
