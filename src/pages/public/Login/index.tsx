04c560d79c53366f1a75270df4fdec9ca7491b12
MuryloSaladino <murylosaladino@gmail.com>
2024-11-21 20:14:24 -0300

    fix address

--- src/pages/public/Login/index.tsx
+++ src/pages/public/Login/index.tsx
@@ -19,7 +19,7 @@ export default function Login() {
         const res = await internalAPI.post("/login", data)
         
         if(res.success) {
-            navigate("/login")
+            navigate("/home")
             return
         }
         res.showMessage()
