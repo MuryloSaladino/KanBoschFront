1e28ce788ce18d287a1f93633f4b53cea6614aa8
MuryloSaladino <murylosaladino@gmail.com>
2024-11-21 22:12:38 -0300

    checkpoint

--- src/pages/private/Home/index.tsx
+++ src/pages/private/Home/index.tsx
@@ -1,20 +1,26 @@
 import Header from "../../../components/Header";
 import useWorkspaces from "./hooks/useWorkspacesByUser";
 import { SMain } from "./styles";
-import { Typography } from "@mui/material";
+import { Container, Stack, Typography } from "@mui/material";
 
 export default function Home() {
 
     const { workspaces } = useWorkspaces()
-
-    console.log(workspaces);
 
     return(
         <>
             <Header/>
 
             <SMain>
-                {workspaces.map(workspace => <Typography>{workspace.name}</Typography>)}
+                <Container>
+                    <Typography 
+                        variant="h5"
+                    >YOUR WORKSPACES</Typography>
+
+                    <Stack>
+                        {workspaces.map(workspace => <Typography>{workspace.name}</Typography>)}
+                    </Stack>
+                </Container>
             </SMain>
         </>
     )
--- src/pages/private/Home/styles.ts
+++ src/pages/private/Home/styles.ts
@@ -3,5 +3,6 @@ import { styled } from "@mui/material";
 export const SMain = styled("div")(({ theme }) => ({
     height: "100vh",
 
-    backgroundColor: theme.palette.grey["900"]
+    backgroundColor: theme.palette.grey["900"],
+    paddingTop: 50 
 }))
