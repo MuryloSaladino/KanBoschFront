705919184c27c0d30325102ba7b6eb76f2a89995
MuryloSaladino <murylosaladino@gmail.com>
2024-12-02 10:11:08 -0300

    feat: workspace card

--- src/pages/private/Home/components/WorkspaceCard/index.tsx
+++ src/pages/private/Home/components/WorkspaceCard/index.tsx
@@ -1,12 +1,23 @@
+import styles from "./styles.module.css"
+import WorkspaceTitle from "@/components/WorkspaceTitle"
 import { IWorkspace } from "@/interfaces/workspaces.interfaces"
+import WorkspaceActions from "../WorkspaceActions"
+import Boards from "../Boards"
 
 interface IWorkspaceCardProps {
     workspace: IWorkspace
 }
 
-export default function WorkspaceCard({  }:IWorkspaceCardProps) {
+export default function WorkspaceCard({ workspace }:IWorkspaceCardProps) {
 
     return(
-        <></>
+        <div className={styles.container}>
+            <div className={styles.container_header}>
+                <WorkspaceTitle name={workspace.name}/>
+                <WorkspaceActions/>
+            </div>
+
+            <Boards boards={workspace.boards}/>
+        </div>
     )
 }
