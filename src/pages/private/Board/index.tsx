6e78718db67597eeea6b3c3b1f355f54637a2357
MuryloSaladino <murylosaladino@gmail.com>
2024-12-02 11:14:53 -0300

    feat: board start page

--- src/pages/private/Board/index.tsx
+++ src/pages/private/Board/index.tsx
@@ -1,8 +1,25 @@
+import styles from "./styles.module.css"
+import Header from "@/components/Header";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import Menu from "./components/Menu";
+
 export default function Board() {
+
+    const { boardId } = useParams()
+    const [asideOpen, setAsideOpen] = useState(window.innerWidth > 500)
+    console.log(asideOpen)
 
     return(
         <>
-            
+            <Header/>
+            <div className={styles.container}>
+                <Menu/>
+
+                <div className={styles.list}>
+                    
+                </div>
+            </div>
         </>
     )
 }
