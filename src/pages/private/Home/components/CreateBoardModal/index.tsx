a8be54cf733ac84b6025c6d8c765813d35fa5e04
MuryloSaladino <murylosaladino@gmail.com>
2024-12-02 11:15:55 -0300

    feat: creation modals

--- src/pages/private/Home/components/CreateBoardModal/index.tsx
+++ src/pages/private/Home/components/CreateBoardModal/index.tsx
@@ -1,7 +1,14 @@
+import Modal from "@/components/Modal"
+import { useContext } from "react"
+import { HomeContext } from "../../context"
+
 export default function CreateBoardModal() {
+
+    const { boardModal } = useContext(HomeContext)
+
     return(
-        <>
+        <Modal open={boardModal.open} onClose={boardModal.hide}>
             
-        </>
+        </Modal>
     )
 }
--- src/pages/private/Home/components/CreateWorkspaceModal/index.tsx
+++ src/pages/private/Home/components/CreateWorkspaceModal/index.tsx
@@ -1,7 +1,14 @@
+import Modal from "@/components/Modal"
+import { HomeContext } from "../../context"
+import { useContext } from "react"
+
 export default function CreateWorkspaceModal() {
+
+    const { workspaceModal } = useContext(HomeContext)
+
     return(
-        <>
+        <Modal open={workspaceModal.open} onClose={workspaceModal.hide}>
             
-        </>
+        </Modal>
     )
 }
