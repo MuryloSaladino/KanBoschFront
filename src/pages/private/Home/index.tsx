fa02ae0c3a30fc8033792166caaee8387dca083b
MuryloSaladino <murylosaladino@gmail.com>
2024-12-02 10:09:35 -0300

    feat: workspaces

--- src/pages/private/Home/index.tsx
+++ src/pages/private/Home/index.tsx
@@ -2,6 +2,8 @@ import CreateBoardModal from "./components/CreateBoardModal";
 import Header from "@/components/Header";
 import useWorkspaces from "./hooks/useWorkspaces";
 import styles from "./styles.module.css"
+import WorkspaceCard from "./components/WorkspaceCard";
+import Text from "@/components/Text";
 
 export default function Home() {
 
@@ -15,7 +17,11 @@ export default function Home() {
             <Header/>
 
             <div className={styles.container}>
-                
+                <Text fontSize="xl2" fontWeight="semibold">Workspaces</Text>
+
+                {workspaces.map(workspace => (
+                    <WorkspaceCard key={workspace.id} workspace={workspace}/>
+                ))}
             </div>
         </>
     )
