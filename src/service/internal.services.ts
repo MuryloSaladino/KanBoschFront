9885fcfe1e77701074e41168551f8ddd4074fc66
MuryloSaladino <murylosaladino@gmail.com>
2024-12-04 14:08:27 -0300

    generic params

--- src/service/internal.services.ts
+++ src/service/internal.services.ts
@@ -2,9 +2,9 @@ import { baseGet, baseDelete, basePatch, basePost, basePut } from ".";
 import { Connections } from "../constants/connections";
 
 export default {
-    get:    (url: string) => baseGet(Connections.InternalAPI + url),
-    post:   <T>(url: string, payload: T) => basePost(Connections.InternalAPI + url, payload),
-    patch:  <T>(url: string, payload: T) => basePatch(Connections.InternalAPI + url, payload),
-    put:    <T>(url: string, payload: T) => basePut(Connections.InternalAPI + url, payload),
-    delete: (url: string) => baseDelete(Connections.InternalAPI + url),
+    get:    <T>(url: string) => baseGet<T>(Connections.InternalAPI + url),
+    post:   <T>(url: string, payload: T) => basePost<T>(Connections.InternalAPI + url, payload),
+    patch:  <T>(url: string, payload: T) => basePatch<T>(Connections.InternalAPI + url, payload),
+    put:    <T>(url: string, payload: T) => basePut<T>(Connections.InternalAPI + url, payload),
+    delete: <T>(url: string) => baseDelete<T>(Connections.InternalAPI + url),
 }
