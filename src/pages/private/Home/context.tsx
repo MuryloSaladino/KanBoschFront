9a15a59b224e4d6f96cd8bf452bd1ea270df1fab
MuryloSaladino <murylosaladino@gmail.com>
2024-12-06 09:15:40 -0300

    feat: add board to context

--- src/pages/private/Home/context.tsx
+++ src/pages/private/Home/context.tsx
@@ -1,4 +1,5 @@
 import useOpen, { IOpenHook } from "@/hooks/useOpen";
+import { IBoard } from "@/interfaces/boards.interfaces";
 import { IWorkspace } from "@/interfaces/workspaces.interfaces";
 import { createContext, ReactNode, useState } from "react";
 
@@ -10,6 +11,7 @@ interface IHomeContext {
     setWorkspaces: (ws: IWorkspace[]) => void
     workspaceId?: string
     setWorkspaceId: (id: string) => void
+    addBoard: (b: IBoard, id: string) => void
 }
 
 export const HomeContext = createContext({} as IHomeContext)
@@ -23,6 +25,14 @@ export function HomeProvider({ children }: { children: ReactNode }) {
     const [workspaceId, setWorkspaceId] = useState<string>()
 
     const addWorkspace = (w: IWorkspace) => setWorkspaces(prev => [...prev, w])
+    const addBoard = (b: IBoard, id: string) => {
+        const index = workspaces.findIndex(w => w.id == id)
+        const workspace = workspaces.splice(index, 1)[0]
+        if(workspace?.boards) {
+            workspace.boards.push(b)
+        }
+        setWorkspaces(prev => [...prev, workspace])
+    }
 
     return (
         <HomeContext.Provider
@@ -33,6 +43,7 @@ export function HomeProvider({ children }: { children: ReactNode }) {
                 workspaceId,
                 workspaces,
                 addWorkspace,
+                addBoard,
                 setWorkspaceId: (id: string) => setWorkspaceId(id),
                 setWorkspaces: (ws: IWorkspace[]) => setWorkspaces(ws)
             }}
