ffb23a3fa9ef9facb2a9a27732c0b1b62997addc
MuryloSaladino <murylosaladino@gmail.com>
2024-12-12 00:51:14 -0300

    card list component

--- src/pages/private/Board/components/CardList/index.tsx
+++ src/pages/private/Board/components/CardList/index.tsx
@@ -1,8 +1,24 @@
-export default function CardList() {
+import Text from "@/components/Text"
+import styles from "./styles.module.css"
+import { ICardList } from "@/interfaces/cards.interfaces"
+import Link from "@/components/Link";
 
+interface ICardListProps {
+    cardList: ICardList
+}
+
+export default function CardList({ cardList }: ICardListProps) {
     return (
-        <div>
-            
-        </div>
+        <li className={styles.container}>
+            <Text fontSize="lg" fontWeight="semibold" color={cardList.color}>{ cardList.name }</Text>
+
+            {cardList.cards.map(card => (
+                <Link type="box" to={`c/${card.id}`}>
+                    <div className={styles.card} key={card.id}>
+                        <Text>{card.description}</Text>
+                    </div>
+                </Link>
+            ))}
+        </li>
     )
 }
